f7c5c7f74cbafb8435ae7637bedb9672011ba9fc
henry22 <henry811010@gmail.com>
2020-05-22 18:19:06 +0800

    Create navtabs component with v-for

--- src/views/RestaurantsFeeds.vue
+++ src/views/RestaurantsFeeds.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="container py-5">
+    <!-- 使用 NavTabs 元件 -->
+    <NavTabs />
     <h1 class="mt-5">最新動態</h1>
   </div>
 </template>
+
+<script>
+import NavTabs from './../components/NavTabs'
+
+export default {
+  components: {
+    NavTabs
+  }
+}
+</script>
