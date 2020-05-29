b639032c5972c2f3350b95ab5d92ab96d99da003
henry22 <henry811010@gmail.com>
2020-05-29 11:10:18 +0800

    Add image upload feature

--- src/views/AdminRestaurantNew.vue
+++ src/views/AdminRestaurantNew.vue
@@ -1,17 +1,25 @@
 <template>
   <div class="container py-5">
     <!-- 餐廳表單 AdminRestaurantForm -->
-    <AdminRestaurantForm />
+    <AdminRestaurantForm @after-submit="handleAfterSubmit" />
   </div>
 </template>
 
 <script>
-import AdminRestaurantForm from './../components/AdminRestaurantForm'
+import AdminRestaurantForm from "./../components/AdminRestaurantForm";
 
 export default {
-  name: 'AdminRestaurantNew',
+  name: "AdminRestaurantNew",
   components: {
     AdminRestaurantForm
+  },
+  methods: {
+    handleAfterSubmit(formData) {
+      // 透過 API 將表單資料送到伺服器
+      for (let [name, value] of formData.entries()) {
+        console.log(`${name}: ${value}`);
+      }
+    }
   }
-}
+};
 </script>
