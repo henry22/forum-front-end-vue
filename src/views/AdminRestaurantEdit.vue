a92b50cb51e034059da75e7308df7393897a654a
henry22 <henry811010@gmail.com>
2020-06-10 14:56:25 +0800

    Create update restaurant api call and add handleAfterSubmit for submitting the form

--- src/views/AdminRestaurantEdit.vue
+++ src/views/AdminRestaurantEdit.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="container py-5">
-    <AdminRestaurantForm @after-submit="handleAfterSubmit" :initial-restaurant="restaurant" />
+    <AdminRestaurantForm
+      @after-submit="handleAfterSubmit"
+      :initial-restaurant="restaurant"
+      :is-processing="isProcessing"
+    />
   </div>
 </template>
 
@@ -26,18 +30,43 @@ export default {
         description: "",
         image: "",
         openingHours: ""
-      }
+      },
+      isProcessing: false
     };
   },
   created() {
     const { id } = this.$route.params;
     this.fetchRestaurant(id);
   },
+  beforeRouteUpdate(to, from, next) {
+    // 路由改變時重新抓取資料
+    const { id } = to.params;
+    this.fetchRestaurant(id);
+    next();
+  },
   methods: {
-    handleAfterSubmit(formData) {
-      // 透過 API 將表單資料送到伺服器
-      for (let [name, value] of formData.entries()) {
-        console.log(`${name}: ${value}`);
+    async handleAfterSubmit(formData) {
+      try {
+        this.isProcessing = true;
+
+        const { data } = await adminApi.restaurants.update({
+          restaurantId: this.restaurant.id,
+          formData
+        });
+
+        if (data.status !== "success") {
+          throw new Error(data.message);
+        }
+
+        this.$router.push({ name: "admin-restaurants" });
+      } catch (error) {
+        console.log("error", error);
+        this.isProcessing = false;
+
+        Toast.fire({
+          icon: "error",
+          title: "無法更新餐廳資料，請稍後再試"
+        });
       }
     },
     // STEP 2: 改成 async...await 語法
