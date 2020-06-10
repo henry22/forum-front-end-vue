f9d5e555310e0a387f454547b6784c4540b07343
henry22 <henry811010@gmail.com>
2020-06-10 12:06:43 +0800

    Create create restaurants api call in AdminRestaurantNew page

--- src/components/AdminRestaurantForm.vue
+++ src/components/AdminRestaurantForm.vue
@@ -89,7 +89,11 @@
         @change="handleFileChange"
       />
     </div>
-    <button type="submit" class="btn btn-primary">送出</button>
+    <button
+      type="submit"
+      class="btn btn-primary"
+      :disabled="isProcessing"
+    >{{isProcessing ? '處理中...' : '送出'}}</button>
   </form>
 </template>
 
@@ -112,6 +116,10 @@ export default {
         image: "",
         openingHours: ""
       })
+    },
+    isProcessing: {
+      type: Boolean,
+      default: false
     }
   },
   data() {
@@ -169,6 +177,22 @@ export default {
       }
     },
     handleSubmit(e) {
+      if (!this.restaurant.name) {
+        Toast.fire({
+          icon: "warning",
+          title: "請填寫餐廳名稱"
+        });
+
+        return;
+      } else if (!this.restaurant.categoryId) {
+        Toast.fire({
+          icon: "warning",
+          title: "請選擇餐廳類別"
+        });
+
+        return;
+      }
+
       const form = e.target;
       const formData = new FormData(form);
 
--- src/views/AdminRestaurantNew.vue
+++ src/views/AdminRestaurantNew.vue
@@ -1,23 +1,46 @@
 <template>
   <div class="container py-5">
     <!-- 餐廳表單 AdminRestaurantForm -->
-    <AdminRestaurantForm @after-submit="handleAfterSubmit" />
+    <AdminRestaurantForm @after-submit="handleAfterSubmit" :is-processing="isProcessing" />
   </div>
 </template>
 
 <script>
 import AdminRestaurantForm from "./../components/AdminRestaurantForm";
+// STEP 1: 匯入 adminAPI 和 Toast
+import adminApi from "./../apis/admin";
+import { Toast } from "./../utils/helpers";
 
 export default {
   name: "AdminRestaurantNew",
   components: {
     AdminRestaurantForm
   },
+  data() {
+    return {
+      isProcessing: false
+    };
+  },
   methods: {
-    handleAfterSubmit(formData) {
-      // 透過 API 將表單資料送到伺服器
-      for (let [name, value] of formData.entries()) {
-        console.log(`${name}: ${value}`);
+    // STEP 2: 改成 async...await 語法
+    async handleAfterSubmit(formData) {
+      try {
+        this.isProcessing = true
+        const { data } = await adminApi.restaurants.create({ formData });
+
+        if (data.status !== "success") {
+          throw new Error(data.message);
+        }
+
+        // STEP 4: 成功的話則轉址到 `/admin/restaurants`
+        this.$router.push({ name: "admin-restaurants" });
+      } catch (error) {
+        this.isProcessing = false
+        // STEP 5: 錯誤處理
+        Toast.fire({
+          icon: "error",
+          title: "無法建立餐廳，請稍後再試"
+        });
       }
     }
   }
