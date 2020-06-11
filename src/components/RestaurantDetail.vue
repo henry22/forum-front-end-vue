6319ca871aaac433800b6046b55431732af10cc1
henry22 <henry811010@gmail.com>
2020-06-11 12:01:21 +0800

    Create getRestaurant api call and watch restaurant value changes and get current user data from vuex state

--- src/components/RestaurantDetail.vue
+++ src/components/RestaurantDetail.vue
@@ -1,28 +1,28 @@
 <template>
   <div class="row">
     <div class="col-md-12 mb-3">
-      <h1>Judy Runte</h1>
-      <p class="badge badge-secondary mt-1 mb-3">義大利料理</p>
+      <h1>{{restaurant.name}}</h1>
+      <p class="badge badge-secondary mt-1 mb-3">{{restaurant.categoryName}}</p>
     </div>
     <div class="col-lg-4">
       <img
         class="img-responsive center-block"
-        src="https://loremflickr.com/320/240/food,dessert,restaurant/"
+        :src="restaurant.image"
         style="width: 250px;margin-bottom: 25px;"
       />
       <div class="contact-info-wrap">
         <ul class="list-unstyled">
           <li>
             <strong>Opening Hour:</strong>
-            08:00
+            {{restaurant.openingHours}}
           </li>
           <li>
             <strong>Tel:</strong>
-            (918) 827-1962
+            {{restaurant.tel}}
           </li>
           <li>
             <strong>Address:</strong>
-            98138 Elisa Road
+            {{restaurant.address}}
           </li>
         </ul>
       </div>
@@ -73,6 +73,14 @@ export default {
     return {
       restaurant: this.initialRestaurant
     };
+  },
+  watch: {
+    initialRestaurant(newValue) {
+      this.restaurant = {
+        ...this.restaurant,
+        ...newValue
+      }
+    }
   },
   methods: {
     addFavorite() {
